9949073921d9334c608e9ed7deee76785f2b525a
Tomislav Sertic <tsertic5@gmail.com>
2020-03-14 19:51:20 +0100

    created colorHellper util which helps generate shades of color palette

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import seedColors from './seedColors';
 import Palette from './containers/Palette';
+import { generatePalette } from './utils/colorHelpers';
 
 function App() {
+  const newPallete = generatePalette(seedColors[4]);
+  console.log(newPallete);
+
   return (
     <div>
       <Palette {...seedColors[4]} />
