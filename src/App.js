1c60c5140d25990c102a3c425c714eb319ee94bb
Tomislav <tsertic5@gmail.com>
2020-03-18 10:43:18 +0000

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import seedColors from './seedColors';
-import Palette from './containers/Palette';
-import { generatePalette } from './utils/colorHelpers';
+import React from "react";
+import seedColors from "./seedColors";
+import Palette from "./containers/Palette";
+import { generatePalette } from "./utils/colorHelpers";
 
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   const genPallete = generatePalette(seedColors[4]);
 
   return (
-    <div>
-      
-      <Palette palette={genPallete} />
-    </div>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={() => {
+          return <h1>Pallet list</h1>;
+        }}
+      />
+      <Route
+        exact
+        path="/palette/:id"
+        render={() => <h1>Individual palette</h1>}
+      />
+      {/*     <div>
+        <Palette palette={genPallete} />
+      </div> */}
+    </Switch>
   );
 }
 
--- src/containers/Palette.js
+++ src/containers/Palette.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import ColorBox from './ColorBox';
-import './Palette.css';
-import Navbar from './Navbar';
-
-
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import "./Palette.css";
+import Navbar from "./Navbar";
 
 class Palette extends Component {
   constructor(props) {
     super(props);
     this.state = {
       level: 500,
-      format:"hex"
+      format: "hex"
     };
   }
 
@@ -18,21 +16,30 @@ class Palette extends Component {
     this.setState({ level: newLevel });
   };
 
-  changeFormat=(val)=>{
-    this.setState({format:val})
-  }
+  changeFormat = val => {
+    this.setState({ format: val });
+  };
 
   render() {
-    console.log(this.props);
-    const { level,format } = this.state;
-    const colorBoxes = this.props.palette.colors[level].map(color => {
-      return <ColorBox background={color[format]} name={color.name} />;
+    const { colors, paletteName, emoji } = this.props.palette;
+    const { level, format } = this.state;
+    const colorBoxes = colors[level].map(color => {
+      return (
+        <ColorBox key={color.id} background={color[format]} name={color.name} />
+      );
     });
     return (
       <div className="Palette">
-        <Navbar level={level} handleChange={this.changeFormat} changeLevel={this.changeLevel} />
-        {/* Navbar goes  here */}
+        <Navbar
+          level={level}
+          handleChange={this.changeFormat}
+          changeLevel={this.changeLevel}
+        />
+
         <div className="Palette-colors">{colorBoxes}</div>
+        <footer className="Palette-footer">
+          {paletteName} <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     );
   }
